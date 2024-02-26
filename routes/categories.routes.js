0a2cb97f83b3e6efa9dfd7459b79b2a538bc173a
Trinankur002 <trinankuratarthi30@gmail.com>
2024-02-27 00:39:33 +0530

    error handler added

--- routes/categories.routes.js
+++ routes/categories.routes.js
@@ -4,54 +4,79 @@ require('dotenv/config')
 const router = express.Router()
 
 router.get(`/`, async (req, res) => {
-    const categoryList = await Category.find();
-    if (!categoryList) {
-        res.status(500).json({success: false})
+    try {
+        const categoryList = await Category.find();
+        if (!categoryList) {
+            res.status(500).json({ success: false })
+        }
+        res.send(categoryList)
+    } catch (error) {
+        console.error(error); // Log the error for investigation
+        return await res.status(500).json({ success: false, message: 'Internal server error' });
     }
-    res.send(categoryList)
 })
 
 router.get(`/find/:id`, async (req, res) => {
-    const category = await Category.findById(req.params.id);
-    if (!category) {
-        res.status(400).json({ success: false , message: 'category with the ID not found'})
+    try {
+        const category = await Category.findById(req.params.id);
+        if (!category) {
+            res.status(400).json({ success: false, message: 'category with the ID not found' })
+        }
+        res.status(200).send(category)
+    } catch (error) {
+        console.error(error); // Log the error for investigation
+        return await res.status(500).json({ success: false, message: 'Internal server error' });
     }
-    res.status(200).send(category)
 })
 
 router.post(`/`, async (req, res) => {
-    let category = new Category({
-        name: req.body.name,
-        color: req.body.color,
-        icon: req.body.icon, 
-        image: req.body.image,
-    })
-    category = await category.save()
+    try {
+        let category = new Category({
+            name: req.body.name,
+            color: req.body.color,
+            icon: req.body.icon,
+            image: req.body.image,
+        })
+        category = await category.save()
 
-    if (!category) { return res.status(400).send('category cannot be read or empty category') }
-    res.send(category)
+        if (!category) { return res.status(400).send('category cannot be read or empty category') }
+        res.send(category)
+    } catch (error) {
+        console.error(error); // Log the error for investigation
+        return await res.status(500).json({ success: false, message: 'Internal server error' });
+    }
 })
 
 router.put(`/:id`, async (req, res) => {
-    const category = await Category.findByIdAndUpdate(
-        req.params.id,
-    {
-        name: req.body.name,
-        color: req.body.color,
-        icon: req.body.icon,
-        image: req.body.image,
-        updatedAt: Date.now(),
-    }, {new : true})
-    if (!category) { return res.status(400).send('category cannot be read or empty category') }
-    res.send(category)
+    try {
+        const category = await Category.findByIdAndUpdate(
+            req.params.id,
+            {
+                name: req.body.name,
+                color: req.body.color,
+                icon: req.body.icon,
+                image: req.body.image,
+                updatedAt: Date.now(),
+            }, { new: true })
+        if (!category) { return res.status(400).send('category cannot be read or empty category') }
+        res.send(category)
+    } catch (error) {
+        console.error(error); // Log the error for investigation
+        return await res.status(500).json({ success: false, message: 'Internal server error' });
+    }
 })
 
 
-router.delete(`/:id`,(req, res) => {
-    Category.findByIdAndDelete(req.params.id).then(category => {
-        if (category) { return res.status(200).json({ success: true, message: 'the category is removed' }) }
-        else { return res.status(404).json({ success: false, message: 'category of that id not found' }) }
-    }).catch(err => { return res.send(400).json({ success: false, error: err }) })
+router.delete(`/:id`,async (req, res) => {
+    try {
+        Category.findByIdAndDelete(req.params.id).then(category => {
+            if (category) { return res.status(200).json({ success: true, message: 'the category is removed' }) }
+            else { return res.status(404).json({ success: false, message: 'category of that id not found' }) }
+        })
+    } catch (error) {
+        console.error(error); // Log the error for investigation
+        return await res.status(500).json({ success: false, message: 'Internal server error' });
+    }
 })
 
 module.exports = router
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,56 +1,42 @@
 const express = require('express')
 require('dotenv/config')
 const { Product } = require('../models/products.model')
+const {Category} = require('../models/category.model')
 const router = express.Router()
 
 router.get(`/`, async (req, res) => {
-    const productList = await Product.find()
-    if (!productList) {
-        res.status(500).json({ success: false })
+    try {
+        const productList = await Product.find()
+        if (!productList) {
+            res.status(500).json({ success: false })
+        }
+        res.send(productList)
+    } catch (error) {
+        console.error(error); // Log the error for investigation
+        return await res.status(500).json({ success: false, message: 'Internal server error' });
     }
-    res.send(productList)
 })
 
 router.get(`/namedescrionimage/`, async (req, res) => {
-    const productList = await Product.find().select('name image description -_id')
-    if (!productList) {
-        res.status(500).json({ success: false })
+    try {
+        const productList = await Product.find().select('name image description -_id')
+        if (!productList) {
+            res.status(500).json({ success: false })
+        }
+        res.send(productList)
+    } catch (error) {
+        console.error(error); // Log the error for investigation
+        return await res.status(500).json({ success: false, message: 'Internal server error' });
     }
-    res.send(productList)
 })
 
 router.post(`/`, async (req, res) => {
-    let product = await new Product({
-        name: req.body.name,
-        image: req.body.image,
-        images: req.body.images,
-        price: req.body.price,
-        countInStock: req.body.countInStock,
-        description: req.body.description,
-        richDescription: req.body.richDescription,
-        brand: req.body.brand,
-        category: req.body.category,
-        rating: req.body.rating,
-        isFeatured : req.body.isFeatured,
-    })
-
-    if (!product) { return res.status(400).send('product cannot be read or empty category') }
-    product = await product.save()
-    res.send(product)
-})
-
-router.get(`/find/:id`, async (req, res) => {
-    const product = await Product.findById(req.params.id).populate('category')
-    if (!product) {
-        res.status(500).json({ success: false })
-    }
-    res.send(product)
-})
-
-router.put(`/:id`, async (req, res) => {
-    const product = await Product.findByIdAndUpdate(
-        req.params.id,
-        {
+    try {
+        const category = await Category.findById(req.body.category);
+        if (!category) {
+            return await res.status(400).json({ success: false, message: 'Category with the ID not found' });
+        }
+        let product = await new Product({
             name: req.body.name,
             image: req.body.image,
             images: req.body.images,
@@ -62,19 +48,72 @@ router.put(`/:id`, async (req, res) => {
             category: req.body.category,
             rating: req.body.rating,
             isFeatured: req.body.isFeatured,
-            updatedAt: Date.now(),
-        }, { new: true })
-    if (!product) { return res.status(400).send('product cannot be read or empty category') }
-    res.send(product)
+        })
+
+        if (!product) { return res.status(400).send('product cannot be read or empty category') }
+        product = await product.save()
+        res.send(product)
+    } catch (error) {
+        console.error(error); // Log the error for investigation
+        return await res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+})
+
+router.get(`/find/:id`, async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id).populate('category')
+        if (!product) {
+            res.status(500).json({ success: false })
+        }
+        res.send(product)
+    } catch (error) {
+        console.error(error); // Log the error for investigation
+        return await res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+})
+
+router.put(`/:id`, async (req, res) => {
+    try {
+        const category = await Category.findById(req.body.category);
+        if (!category) {
+            return await res.status(400).json({ success: false, message: 'Category with the ID not found' });
+        }
+        const product = await Product.findByIdAndUpdate(
+            req.params.id,
+            {
+                name: req.body.name,
+                image: req.body.image,
+                images: req.body.images,
+                price: req.body.price,
+                countInStock: req.body.countInStock,
+                description: req.body.description,
+                richDescription: req.body.richDescription,
+                brand: req.body.brand,
+                category: req.body.category,
+                rating: req.body.rating,
+                isFeatured: req.body.isFeatured,
+                updatedAt: Date.now(),
+            }, { new: true })
+        if (!product) { return await res.status(400).send('product cannot be read or empty category') }
+        res.send(product)
+    } catch (error) {
+        console.error(error); // Log the error for investigation
+        return await res.status(500).json({ success: false, message: 'Internal server error' });
+    }
 })
 
 router.delete(`/:id`, async (req, res) => {
-    const product = await Product.findById(req.params.id)
-    if (!product) {
-        res.status(500).json({ success: false })
+    try {
+        const product = await Product.findById(req.params.id)
+        if (!product) {
+            res.status(500).json({ success: false })
+        }
+        await Product.findOneAndDelete(req.params.id)
+        res.send('product deleted successfully')
+    } catch (error) {
+        console.error(error); // Log the error for investigation
+        return await res.status(500).json({ success: false, message: 'Internal server error' });
     }
-    await Product.findOneAndDelete(req.params.id)
-    res.send('product deleted successfully')
 })
 
 
